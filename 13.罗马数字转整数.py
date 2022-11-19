615913100380a32000872087ac561ce7b9078aba
lulei2147 <820365542@qq.com>
2022-11-19 19:44:11 +0800

    新算法

--- 13.罗马数字转整数.py
+++ 13.罗马数字转整数.py
@@ -7,27 +7,20 @@
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
-        valDict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
-        'a':900, 'b':400, 'c':90, 'd':40, 'e':9, 'f':4}
-
-        # IV: 4    IX: 9
-        # XL: 40   XC: 90
-        # CD: 400  CM: 900
-        resStr = s.replace("CM", "a")
-        resStr =resStr.replace("CD", "b")
-        resStr =resStr.replace("XC", "c")
-        resStr =resStr.replace("XL", "d")
-        resStr =resStr.replace("IX", "e")
-        resStr =resStr.replace("IV", "f")
+        valDict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
         resultVal = 0
-        resultList = list(resStr)
+        strEnum = enumerate(s)
+        sLen = len(s)
 
-        for item in resultList:
-            resultVal += valDict[item]
+        for idx, item in strEnum:
+            if idx < sLen - 1 and valDict[s[idx]] < valDict[s[idx + 1]]:
+                resultVal -= valDict[s[idx]]
+            else:
+                resultVal += valDict[s[idx]]
 
         return resultVal
         
 # @lc code=end
 
-print(Solution().romanToInt("LVIII"))
+print(Solution().romanToInt("MCMXCIV"))
