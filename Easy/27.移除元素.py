5df6ae9f0e0a2756f6356e32d7ffcd8860979bce
lulei2147 <820365542@qq.com>
2022-11-24 13:20:44 +0800

    两个指针，一个指向当前要替换位置，一个指向当前遍历的位置

---
+++ Easy/27.移除元素.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        numsLen = len(nums)
+        curIdx = 0
+        repIdx = -1
+
+        while curIdx < numsLen:
+            if nums[curIdx] == val:
+                if repIdx == -1:
+                    repIdx = curIdx
+
+                curIdx += 1
+            else:
+                if repIdx != -1:
+                    nums[repIdx] = nums[curIdx]
+                    repIdx += 1
+                
+                curIdx += 1
+
+        if repIdx == -1:
+            repIdx = curIdx
+
+        return repIdx
+# @lc code=end
+
+Solution().removeElement([], 4)
+
